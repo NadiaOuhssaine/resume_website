7c9254993e58e10e28090aec7113219b5e623b5b
Nadia <ouhssaine.nadia@gmail.com>
2021-05-08 19:48:51 +0200

    Add contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ResumePageComponent } from './resume-page/resume-page.component';
-import { ApiPageComponent } from './api-page/api-page.component';
+import { ApiPageComponent} from './api-page/api-page.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -24,7 +28,10 @@ import { PortfolioPageComponent } from './portfolio-page/portfolio-page.componen
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    MatInputModule,
+    NoopAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
